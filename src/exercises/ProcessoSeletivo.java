33dd058f9cf8faa3a292d596c27df879a86cf250
Allan <allan.mendes0976@gmail.com>
2025-01-27 22:02:18 -0300

    feat: update exercise

--- src/exercises/ProcessoSeletivo.java
+++ src/exercises/ProcessoSeletivo.java
@@ -1,4 +1,5 @@
 package exercises;
+import java.util.Random;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
@@ -10,8 +11,41 @@ public class ProcessoSeletivo {
 
         //selecaoCandidatos();
 
-        imprimirSelecionados();
+        //imprimirSelecionados();
 
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        for(String candidato: candidatos){
+            entrarEmContato(candidato);
+        }
+
+    }
+
+    static void entrarEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if (continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO.");
+            }
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA.");
+        } else {
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÙMERO MÁXIMO DE TENTATIVAS " +  tentativasRealizadas + " REALIZADA.");
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void selecaoCandidatos() {
