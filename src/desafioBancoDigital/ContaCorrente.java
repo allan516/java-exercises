b82ef707db1b5a362b69336a01c086bea1a9550c
Allan <allan.mendes0976@gmail.com>
2025-02-12 11:09:40 -0300

    feat: create exercise desafioBancoDigital

---
+++ src/desafioBancoDigital/ContaCorrente.java
@@ -0,0 +1,25 @@
+package desafioBancoDigital;
+
+public class ContaCorrente {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar() {}
+
+    public void depositar() {}
+
+    public void transferir() {}
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+}
---
+++ src/desafioBancoDigital/ContaPoupanca.java
@@ -0,0 +1,13 @@
+package desafioBancoDigital;
+
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar() {}
+
+    public void depositar() {}
+
+    public void transferir() {}
+}
