a8289cd3af3f2daad7928ac168f058c1e5d668c0
Allan <allan.mendes0976@gmail.com>
2025-03-06 11:03:33 -0300

    feat: finish exercise

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -20,10 +20,39 @@ public class Main {
         mentoria1.setDescricao("Descrição mentoria Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devAllan = new Dev();
+        devAllan.setNome("Allan");
+        devAllan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devAllan.getConteudosInscritos());
+        devAllan.progredir();
+        devAllan.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos inscritos: " + devAllan.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + devAllan.getConteudosConcluidos());
+        System.out.println("XP: " + devAllan.calcularXp());
+
+        System.out.println("___________________________________________________________");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularXp());
 
     }
 }
