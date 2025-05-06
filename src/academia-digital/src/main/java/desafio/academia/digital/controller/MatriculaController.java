cf49bab39b3455f52c3a9f37325ef87281e029ed
Allan <allan.mendes0976@gmail.com>
2025-05-06 00:38:36 -0300

    feat: create challenge

---
+++ src/academia-digital/src/main/java/desafio/academia/digital/controller/MatriculaController.java
@@ -0,0 +1,29 @@
+package desafio.academia.digital.controller;
+
+import desafio.academia.digital.entity.Matricula;
+import desafio.academia.digital.entity.form.MatriculaForm;
+import desafio.academia.digital.service.impl.MatriculaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/matriculas")
+public class MatriculaController {
+
+    @Autowired
+    private MatriculaServiceImpl service;
+
+    @PostMapping
+    public Matricula create(@Valid @RequestBody MatriculaForm form) {
+        return service.create(form);
+    }
+
+    @GetMapping
+    public List<Matricula> getAll(@RequestParam(value = "bairro", required = false) String bairro) {
+        return service.getAll(bairro);
+    }
+
+}
---
+++ src/academia-digital/src/main/java/desafio/academia/digital/entity/form/AvaliacaoFisicaUpdateForm.java
@@ -0,0 +1,8 @@
+package desafio.academia.digital.entity.form;
+
+public class AvaliacaoFisicaUpdateForm {
+
+  private double peso;
+
+  private double altura;
+}
